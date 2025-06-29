0ad53cc14e0ed9ac982b9f12c9c1af6e1984023f
jxnfernandx <jma240002@utdallas.edu>
2025-06-29 12:08:38 -0500

    Added careers json and page

---
+++ client/util/careers.js
@@ -0,0 +1,29 @@
+import { title } from "process";
+
+export default careers = [
+	{
+		title: "Software Engineer",
+		description: "Join our team to build innovative healthcare solutions.",
+		location: "Remote",
+		date: "2025-10-01",
+	},
+	{
+		title: "Data Scientist",
+		description: "Analyze healthcare data to improve patient outcomes.",
+		location: "New York, NY",
+		date: "2025-10-15",
+	},
+	{
+		title: "Product Manager",
+		description: "Lead product development for our healthcare platform.",
+		location: "San Francisco, CA",
+		date: "2025-11-01",
+	},
+	{
+		title: "UX/UI Designer",
+		description: "Design user-friendly interfaces for our applications.",
+		location: "Remote",
+		date: "2025-11-15",
+	},
+
+];
